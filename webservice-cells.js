71b48c15d3e18673dc0886193ea34b0280cf5e3d
GERKOK <GERKOK@GERKOK.group2000.nl>
2017-12-13 09:03:57 +0100

    fixed package and zookeeper

--- webservice-cells.js
+++ webservice-cells.js
@@ -73,7 +73,7 @@ function addZookeeper(callback){
     //adds Service to zookeeper under <servicename>/UUID
      logger.log('info','Creating Zookeeper service '  + config.service.name+ '/' +uuid );
      
-     kClient.exists("/dropwizard",function(err,ret){
+     zkClient.exists("/dropwizard",function(err,ret){
          if(!ret){
         	 zkClient.create("/dropwizard", function (error) {
 	         if (error) {
@@ -85,7 +85,7 @@ function addZookeeper(callback){
          }
     });
      
-     kClient.exists("/dropwizard/services",function(err,ret){
+     zkClient.exists("/dropwizard/services",function(err,ret){
          if(!ret){
         	 zkClient.create("/dropwizard/services", function (error) {
 	         if (error) {
